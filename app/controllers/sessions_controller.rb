5af1426b8c24556ae5fc4daa30e6bd0dc5d363d0
arifinsukses5yeah@gmail.com <arifinsukses5yeah@gmail.com>
2024-02-21 09:40:39 +0700

    menu login sudah muncul

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,5 @@
-class HomeController < ApplicationController
-    def index
-        if session[:user]
-          # If the user is logged in, display the menu
-          @menu_visible = true
-        else
-          # If the user is not logged in, keep the menu hidden
-          @menu_visible = false
-        end
-    end
-  
+# app/controllers/sessions_controller.rb
+class SessionsController < ApplicationController
     def login
       username = params[:username]
       password = params[:password]
@@ -16,13 +7,13 @@ class HomeController < ApplicationController
   
       # Add your authentication logic here
       # For example, you can check if the username and password are correct and match a user in your database
-  
       if authenticate(username, password, hak_akses)
-        # Set the session variable to indicate that the user is logged in
+        # If the login is successful, set a session variable to indicate that the user is logged in
         session[:user] = { username: username, hak_akses: hak_akses }
+        # Redirect the user to the index action of the HomeController
         redirect_to root_path
       else
-        # Display an error message if the login fails
+        # If the login fails, display an error message
         flash[:error] = "Invalid username or password"
         render :index
       end
@@ -32,7 +23,7 @@ class HomeController < ApplicationController
   
     def authenticate(username, password, hak_akses)
       # Add your authentication logic here
-      # Return true if the username and password are correct, and the hak_akses is valid
+      # Return true if the username, password, and hak_akses are correct
       # Return false otherwise
       true
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 # config/routes.rb
 Rails.application.routes.draw do
   root 'home#index'
+  post '/login', to: 'sessions#login'
 end
